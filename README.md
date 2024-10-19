1a5854c44271b9cf5dcc2d6d86cca1a47f6e9a61
Raufmedetov <mdtovrovsn@gmail.com>
2024-10-19 21:02:24 +0400

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ChatGPT-Clone
+# ChatGPT-Clone
--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const chatContainer = document.querySelector(".chat-container");
 const themeButton = document.querySelector("#theme-btn");
 const deleteButton = document.querySelector("#delete-btn");
 
-const API_KEY = "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";
+// const API_KEY="sk-bS3QZeVB8xboF9iRyQRzbkADOqwl6VmjnNunLB2DGfT3BlbkFJoNeVJy0kHK3poDqIdhqMZvVfW9phG4KO4vr8zT_VcA";
 
 let userText = null;
 
